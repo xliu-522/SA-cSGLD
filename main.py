a10e33de0fa65a3f0ef75212d88c0cb4c88e5c67
xliu-522 <xinruliu@bu.edu>
2023-09-15 14:03:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import argparse
 import json
 import torch
+from torch import nn
 from src.data import Data
-from src.train import mcmc_train
-#import importlib
-#import model.models
+from src.train import mcmc_train_test
 import models
+import numpy as np
 #from unique_names_generator import get_random_name
 
 def main():
@@ -45,9 +45,27 @@ def main():
         print(f"Model {model_name} not found.")
 
     model = model.to(device)
-    params = model.parameters()
-    trainer = mcmc_train(device=device, params=params, data=data_obj.train_dataloader, config=config)
-    trainer.train_it()
+    
+    # Dummy batch size, number of channels, height, and width
+    batch_size = 128  # Adjust as needed
+    num_channels = 3
+    height = 224
+    width = 224
+
+    # Create a random dummy batch with the specified shape
+    dummy_batch = torch.randn(batch_size, num_channels, height, width).to(device)
+
+    model(dummy_batch)
+
+    config["model"]["total_par"] = sum(P.numel() for P in model.parameters() if P.requires_grad)
+
+    trainer = mcmc_train_test(device=device, train_data=data_obj.train_dataloader, test_data = data_obj.test_dataloader, config=config, model=model)
+    epoches = config["training"]["epoches"]
+    print("**** Start training ****")
+    for t in range(epoches):
+        print(f"Epoch {t+1}\n-------------------------------")
+        trainer.train_it()
+        trainer.test_it()
 
 
 
