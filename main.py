63e75b4ab5bbed95d48b9f242958043acf730b68
xliu-522 <xinruliu@bu.edu>
2023-09-15 10:15:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import json
 import torch
 from src.data import Data
 from src.train import mcmc_train
-from model.AlexNet import AlexNet
+#import importlib
+#import model.models
+import models
 #from unique_names_generator import get_random_name
 
 def main():
@@ -30,11 +32,22 @@ def main():
     print("**** Reading data ****")
     data_obj = Data(config)
     data_obj.read()
+    config["data"]["sample_size"] = len(data_obj.train_dataset)
 
+    print("**** Load Model ****")
+    model_name = config["model"]["model_name"]
+    model_reference = getattr(models, model_name)
+    if model_reference is not None and callable(model_reference):
+        # Create an instance of the class
+        model = model_reference()
+        print(f"Found model {model_name}. ")
+    else:
+        print(f"Model {model_name} not found.")
 
-    model = AlexNet().to(device)
+    model = model.to(device)
     params = model.parameters()
     trainer = mcmc_train(device=device, params=params, data=data_obj.train_dataloader, config=config)
+    trainer.train_it()
 
 
 
