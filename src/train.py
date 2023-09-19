b6f15d05c6c1292dca4c49867c0fded7c6af20ba
xliu-522 <xinruliu@bu.edu>
2023-09-19 11:09:29 -0400

    Update train.py

--- src/train.py
+++ src/train.py
@@ -3,7 +3,7 @@ import torch
 import random
 from scipy.special import expit, logit
 from torch import nn
-from torch.func import functional_call, grad
+#from torch.func import functional_call, grad
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
@@ -38,6 +38,7 @@ class mcmc_train_test(object):
             loss.backward()
             self.sampler.update_params()
             self.sampler.select_CoordSet()
+            self.sampler.update_sparsity()
             self.sampler.zero_grad()
 
     def test_it(self):
@@ -56,3 +57,4 @@ class mcmc_train_test(object):
         test_loss /= num_batches
         correct /= test_size
         print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
