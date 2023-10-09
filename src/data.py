60949eca2512a9507f213634475d8db8ef6d80d8
xliu-522 <xinruliu@bu.edu>
2023-10-08 22:19:13 -0400

    Update data.py

--- src/data.py
+++ src/data.py
@@ -18,7 +18,6 @@ class Data(object):
         train_dataset = datasets.CIFAR10(root=self.data_path, train=True, download=True)
         self.data_mean = (train_dataset.data / 255.0).mean(axis=(0,1,2))
         self.data_std = (train_dataset.data / 255.0).std(axis=(0,1,2))
-        print(self.data_mean)
         data_transform = transforms.Compose([transforms.ToTensor(), resize_transform,transforms.Normalize(self.data_mean, self.data_std)])
 
         # Loading the training dataset. 
@@ -27,6 +26,6 @@ class Data(object):
         self.test_dataset = datasets.CIFAR10(root=self.data_path, train=False, transform=data_transform, download=True)
 
         # We define a set of data loaders that we can use for various purposes later.
-        self.train_dataloader = torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, drop_last=True, pin_memory=True, num_workers=4)
-        self.test_dataloader = torch.utils.data.DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, drop_last=False, num_workers=4)
+        self.train_dataloader = torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, drop_last=True, pin_memory=True, num_workers=1)
+        self.test_dataloader = torch.utils.data.DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, drop_last=False, num_workers=1)
         
