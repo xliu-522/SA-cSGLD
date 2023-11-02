18c4575cf367aeb4a9f024f2e2274aebefe8629a
xliu-522 <xinruliu@bu.edu>
2023-11-01 20:55:53 -0400

    Update sampler.py

--- src/sampler.py
+++ src/sampler.py
@@ -12,9 +12,10 @@ from types import SimpleNamespace
 
 class OptimizerTemplate:
 
-    def __init__(self, params, lr):
+    def __init__(self, params, lr, lr0):
         self.params = list(params)
         self.lr = lr
+        self.lr0 = lr0
 
     def zero_grad(self):
         ## Set gradients of all parameters to zero
@@ -47,19 +48,23 @@ class SGD(OptimizerTemplate):
         
 class SGLD(OptimizerTemplate):
 
-    def __init__(self, params, lr, spleSize):
-        super().__init__(params, lr)
+    def __init__(self, params, lr, lr0, spleSize):
+        super().__init__(params, lr, lr0)
         self.spleSize = spleSize
     
     def update_param(self, p):
         gauss = torch.distributions.Normal(torch.zeros_like(p.data),torch.ones_like(p.data)).sample()
-        update = -self.spleSize*self.lr * p.grad + np.sqrt(2*self.lr)*gauss
+        #sub_grad = -self.spleSize*p.grad - 1*p.data
+        #update =  0.5*self.lr*sub_grad + np.sqrt(self.lr)*gauss
+        update = -self.spleSize*self.lr * p.grad + np.sqrt(2*self.lr0)*gauss
+        
         p.add_(update) # In-place update => saves memory and does not create computation graph
         
 class SASGLD(object):
     def __init__(self, device, config, model):
         self.device = device
         self.model = model
+        
         #self.lr = torch.tensor(config["sampler"]["learning_rate"], dtype=torch.float32).to(self.device)
         self.gamma = torch.tensor(config["training"]["gamma"], dtype=torch.float32).to(self.device)
         self.rho_0 = torch.tensor(config["sampler"]["rho_0"], dtype=torch.float32).to(self.device)
@@ -70,17 +75,19 @@ class SASGLD(object):
         self.update_rate = config["sampler"]["update_rate"]
         self.batch_size = config["data"]["batch_size"]
         self.loss_fn = nn.CrossEntropyLoss().to(self.device)
+        self.sparse_size = [int(np.ceil(self.update_rate*np.prod(P.shape))) for P in self.model.parameters()]
+        print(self.sparse_size)
         # Initiate parameter structure
         self.params_struct, self.J_vec, self.CoordSet  = {}, {}, {}
+        self.layersize = []
         i = 0
         for P in self.model.parameters():
             if P.requires_grad:
                 self.params_struct[i] = torch.ones(size=P.shape)
-                self.J_vec[i] = torch.ceil(self.update_rate*torch.prod(torch.tensor(P.shape)))
-                self.CoordSet[i] = torch.zeros(self.J_vec[i].numel())
+                self.layersize.append(torch.tensor(np.prod(P.shape), dtype=torch.float32).to(self.device))
             i+=1
         
-        self.a = self.u * torch.log(self.total_par) + 0.5*torch.log(self.rho_0/self.rho_1)
+        self.a = (self.u+1) * torch.log(self.total_par) + 0.5*torch.log(self.rho_0/self.rho_1)
     
 
     def zero_grad(self, model):
@@ -98,13 +105,16 @@ class SASGLD(object):
             if P.grad is None: # We skip parameters without any gradients
                 i+=1
                 continue
-            self.CoordSet[i] = np.random.choice(a=P.numel(), size = int(self.J_vec[i].item()), replace = False)
+            #self.CoordSet[i] = np.random.choice(a=P.numel(), size = int(self.J_vec[i].item()), replace = False)
+            self.CoordSet[i] = np.random.choice(a=P.numel(), size = self.sparse_size[i], replace = False)
+            self.params_struct[i].reshape(-1)[self.CoordSet[i]] = 0
             i+=1
 
 
     @torch.no_grad()
-    def update_params(self, model):
+    def update_params(self, model, lr):
         ## Update all parameters
+        lr = torch.tensor(lr, dtype=torch.float32).to(self.device)
         i=0
         for P in model.parameters():
             if P.grad is None: # We skip parameters without any gradients
@@ -113,7 +123,7 @@ class SASGLD(object):
             gauss = torch.distributions.Normal(torch.zeros_like(P.data), torch.ones_like(P.data)).sample()
             P[self.params_struct[i]==0].data = torch.sqrt(1/self.rho_0)*gauss[self.params_struct[i]==0]
             index = self.params_struct[i]==1
-            g = - self.gamma * ( self.rho_1*P.data[index] + self.spleSize*P.grad[index]) + torch.sqrt(2*self.gamma)*gauss[index]
+            g = - lr * (self.rho_1*P.data[index] + self.spleSize*P.grad[index]) + torch.sqrt(2*self.gamma)*gauss[index]
             #g = -self.spleSize*self.gamma * P.grad[index] + torch.sqrt(2*self.gamma)*gauss[index]
             P.data[index]+=g
             i+=1
@@ -124,21 +134,26 @@ class SASGLD(object):
         ## Update all selected parameters structures
         i=0
         sparse_sum = 0.0
+        scale = 10
         for P in model.parameters():
             if P.grad is None: # We skip parameters without any gradients
                 i+=1
                 continue
-            Grad = P.grad.reshape(-1)[self.CoordSet[i]]
+            Grad = scale*P.grad.reshape(-1)[self.CoordSet[i]]
             Weights = P.data.reshape(-1)[self.CoordSet[i]]
-            zz1 = self.a + 0.5*(self.rho_1 - self.rho_0)*Weights**2 -self.spleSize * -Grad*Weights
-            prob = torch.sigmoid(-zz1)
+            zz1 = -Weights*self.spleSize*Grad
+            zz2 = 0.5*(self.rho_1 - self.rho_0)*Weights**2
+            zz = self.a -zz1 + zz2 - 0.5*(zz1**2)
+            #zz = self.a + 0.5*(self.rho_1 - self.rho_0)*Weights**2 -self.spleSize * -Grad*Weights
+            prob = torch.sigmoid(-zz)
             bern = torch.distributions.Binomial(1, prob).sample()
+            #self.params_struct[i].reshape(-1)[self.CoordSet[i]] = bern
             params_struct_temp = self.params_struct[i].reshape(-1).to(self.device)
             params_struct_temp[self.CoordSet[i]] = bern
             self.params_struct[i] = params_struct_temp.reshape(self.params_struct[i].shape)
             sparse_sum += torch.sum(self.params_struct[i])
             i+=1
-        print("sparsity: ", (sparse_sum/self.total_par).item())
+        #print("sparsity: ", (sparse_sum/self.total_par).item())
         return (sparse_sum/self.total_par).item()
     
     
@@ -159,8 +174,33 @@ class SASGLD(object):
             if P.grad is None: # We skip parameters without any gradients
                 i+=1
                 continue
-            gauss = torch.distributions.Normal(torch.zeros_like(P.data), torch.ones_like(P.data)).sample()
-            P[self.params_struct[i]==0].data = gauss[self.params_struct[i]==0]
-            self.params_struct[i]=torch.ones(size=self.params_struct[i].shape)
+            L = torch.sum(self.params_struct[i]==0)
+            if L > 0:
+                gauss = torch.distributions.Normal(torch.zeros_like(P.data), torch.sqrt(1/self.layersize[i])*torch.ones_like(P.data)).sample()
+                P[self.params_struct[i]==0].data = gauss[self.params_struct[i]==0]
+                self.params_struct[i]=torch.ones(size=self.params_struct[i].shape)
+        return model
+    
+    def calculate_sparsity(self, model):
+        i = 0
+        sparse_sum = 0
+        for P in model.parameters():
+            if P.grad is None: # We skip parameters without any gradients
+                i+=1
+                continue
+            sparse_sum += torch.sum(self.params_struct[i])
+            i+=1
+        sparse_sum = (sparse_sum/self.total_par).item() 
+        return sparse_sum
+    
+    def exploration(self, model):
+        i = 0
+        
+        for P in model.parameters():
+            if P.grad is None: # We skip parameters without any gradients
+                i+=1
+                continue
+            g = - self.gamma * (self.rho_1*P.data + self.spleSize*P.grad)
+            P.data+=g
             i+=1
         return model
