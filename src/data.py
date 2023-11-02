e93c4c4165660ac69637f285825997131483d74b
xliu-522 <xinruliu@bu.edu>
2023-11-01 20:55:51 -0400

    Update data.py

--- src/data.py
+++ src/data.py
@@ -12,18 +12,25 @@ class Data(object):
         self.data_path = config['data']['data_path']
         self.batch_size =config['data']['batch_size']
         self.image_size = config['data']['image_size']
+        self.model_name = config['model']['model_name']
 
     def read(self):
         resize_transform = transforms.Resize(size=(self.image_size,self.image_size),antialias=True)
         train_dataset = datasets.CIFAR10(root=self.data_path, train=True, download=True)
         self.data_mean = (train_dataset.data / 255.0).mean(axis=(0,1,2))
         self.data_std = (train_dataset.data / 255.0).std(axis=(0,1,2))
-        data_transform = transforms.Compose([transforms.ToTensor(), resize_transform,transforms.Normalize(self.data_mean, self.data_std)])
 
+        if self.model_name == "AlexNet":
+            data_transform = transforms.Compose([transforms.ToTensor(), resize_transform,transforms.Normalize(self.data_mean, self.data_std)])
+        else:
+            data_transform = transforms.ToTensor()
+            
         # Loading the training dataset. 
-        self.train_dataset = datasets.CIFAR10(root=self.data_path, train=True, transform=data_transform, download=True)
+        self.train_dataset = datasets.CIFAR10(root=self.data_path, train=True, transform=data_transform , download=True)
+        
         # Loading the test set
-        self.test_dataset = datasets.CIFAR10(root=self.data_path, train=False, transform=data_transform, download=True)
+        self.test_dataset = datasets.CIFAR10(root=self.data_path, train=False, transform=data_transform , download=True)
+        
 
         # We define a set of data loaders that we can use for various purposes later.
         self.train_dataloader = torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, drop_last=True, pin_memory=True, num_workers=1)
