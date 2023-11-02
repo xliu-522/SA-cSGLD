2e55fe16f013aaf2a578b86aa13937bc7bca3048
xliu-522 <xinruliu@bu.edu>
2023-11-01 20:55:55 -0400

    Update train.py

--- src/train.py
+++ src/train.py
@@ -12,7 +12,6 @@ from torchvision import transforms
 from types import SimpleNamespace
 from src.sampler import SGD, SGLD, SASGLD
 
-
 class mcmc_train_test(object):
     def __init__(self, device, res_dir, train_data, test_data, config, model):
         self.config = config
@@ -20,18 +19,23 @@ class mcmc_train_test(object):
         self.train_dataloader = train_data
         self.test_dataloader = test_data
         self.model = model
+        self.data_path = config['data']['data_path']
+        self.image_size = config['data']['image_size']
+        self.model_name = config['model']['model_name']
         self.sparse = config["sampler"]["sparse"]
         self.sampler_name = self.config["sampler"]["sampler"]
         self.sample_size = config["data"]["sample_size"]
         self.update_rate = config["sampler"]["update_rate"]
-        self.total_par = config["model"]["total_par"]
+        #self.total_par = config["model"]["total_par"]
         self.batch_size = config["data"]["batch_size"]
         self.num_batch = self.sample_size//self.batch_size
         self.cycles = config["training"]["cycles"]
+        self.numIter = config["training"]["numIter"]
         self.epoches = config["training"]["epoches"]
         self.threshold = config["training"]["threshold"]
-        self.lr = config["training"]["gamma"]
-        self.loss_fn = nn.CrossEntropyLoss().to(self.device)
+        self.lr0 = config["training"]["gamma"]
+        self.alpha_0 = config["training"]["alpha_0"]
+        self.loss_fn = nn.CrossEntropyLoss()
         self.Loss, self.Sparsity, self.Acc = [], [], []
         self.res_dir = res_dir
        
@@ -43,102 +47,120 @@ class mcmc_train_test(object):
             for t in range(self.epoches):
                 print(f"Epoch {t+1}\n-------------------------------")
                 for batch, (X, y) in enumerate(self.train_dataloader):
-                    print("Batch: ", batch)
                     X = X.to(self.device)
                     y = y.to(self.device)
                     self.model = self.sampler.sparsify_model_params(self.model)
                     pred = self.model(X)
                     loss = self.loss_fn(pred, y)
                     loss.backward()
-                    self.model = self.sampler.update_params(self.model)
+                    self.model = self.sampler.update_params(self.model, self.lr0)
                     self.sampler.select_CoordSet(self.model)
+                    
+                    # calculate 2nd gradient calculate
+                    pred = self.model(X)
+                    loss = self.loss_fn(pred, y)
+                    loss.backward()
                     self.spar = self.sampler.update_sparsity(self.model)
                     self.model = self.sampler.zero_grad(self.model)
                     if iters % 100 == 0:
                         self.test_it()
+                    if iters % 10000 == 0:
                         print('save!')
                         self.model.cpu()
                         torch.save(self.model.state_dict(),f'{self.res_dir}/model_{iters}.pt')
                         self.model.cuda(self.device)
-                    self.test_it()
                     iters+=1
+                    
         
         elif self.sampler_name == "sacsgld":
             num_iter = self.num_batch*self.epoches
-            iter_C = num_iter//self.cycles
-            iters = 1
+            cyclelen = num_iter//self.cycles
+            kk = 1
             C = 1
             self.sampler = SASGLD(device=self.device, config=self.config, model=self.model)
             for epoch in range(self.epoches):
                 print(f"Epoch {epoch+1}\n-------------------------------")
                 for batch, (X, y) in enumerate(self.train_dataloader):
-                    print("Batch: ", batch)
                     X = X.to(self.device)
                     y = y.to(self.device)
-                    r_k = ((k-1) % iter_C)/iter_C
-                    if r_k == 0:
+                    r = ((kk-1) % cyclelen)/cyclelen
+                    alpha = max(self.alpha_0/2 * (math.cos(math.pi * r) + 1), self.threshold)
+                    lr = self.lr0*alpha
+                    if r == 0:
+                        self.model = self.sampler.reinit_sparsity(self.model)
+                    if r < 0.0:
+                        self.model = self.sampler.exploration(self.model)
+                        self.spar = self.sampler.calculate_sparsity(self.model)
+                    else:
+                        self.model = self.sampler.sparsify_model_params(self.model)
+                        pred = self.model(X)
+                        loss = self.loss_fn(pred, y)
+                        loss.backward()
+                        self.model = self.sampler.update_params(self.model, lr)
+                        self.sampler.select_CoordSet(self.model)
+
+                        # calculate 2nd gradient calculate
+                        pred = self.model(X)
+                        loss = self.loss_fn(pred, y)
+                        loss.backward()
+                        self.spar = self.sampler.update_sparsity(self.model)
+                        self.model = self.sampler.zero_grad(self.model)
+                    if r == 0 and kk != 1:
                         print('save!')
                         self.model.cpu()
                         torch.save(self.model.state_dict(),f'{self.res_dir}/model_cycle{C}.pt')
                         C += 1
                         self.model.cuda(self.device)
-                    a_k = max(self.lr/2 * (math.cos(math.pi * r_k) + 1), self.threshold)
-                    print('r_k: ', r_k)
-                    print('a_k: ', a_k)
-                    self.model = self.sampler.sparsify_model_params(self.model)
-                    pred = self.model(X)
-                    loss = self.loss_fn(pred, y)
-                    loss.backward()
-                    self.model = self.sampler.update_params(self.model)
-                    self.sampler.select_CoordSet(self.model)
-                    self.spar = self.sampler.update_sparsity(self.model)
-                    self.model = self.sampler.zero_grad(self.model)
-                    if a_k == 0:
-                        self.model = self.sampler.reinit_sparsity(self.model)
                         
-                    if iters % 100 == 0:
+                    if kk % 100 == 0:
                         self.test_it()
-                    iters+=1
+                        print('r: ', r)
+                        print('alpha: ', alpha)
+                        print('lr:', lr)
+                    kk+=1
 
         elif self.sampler_name == "csgld":
             num_iter = self.num_batch*self.epoches
-            iter_C = num_iter//self.cycles
-            iters = 1
+            cyclelen = num_iter//self.cycles
+            kk = 1
             C = 1
-            self.sampler = SASGLD(device=self.device, config=self.config, model=self.model)
+            #self.sampler = SASGLD(device=self.device, config=self.config, model=self.model)
             for epoch in range(self.epoches):
                 print(f"Epoch {epoch+1}\n-------------------------------")
                 for batch, (X, y) in enumerate(self.train_dataloader):
-                    print("Batch: ", batch)
+                    # print("Batch: ", batch)
                     X = X.to(self.device)
                     y = y.to(self.device)
-                    r_k = ((iters-1) % iter_C)/iter_C
-                    if r_k == 0:
+                    r = ((kk-1) % cyclelen)/cyclelen
+                    alpha = max(self.alpha_0/2 * (math.cos(math.pi * r) + 1), self.threshold)
+                    lr = self.lr0*alpha
+                    if r == 0 and kk != 1:
                         print('save!')
                         self.model.cpu()
                         torch.save(self.model.state_dict(),f'{self.res_dir}/model_cycle{C}.pt')
                         C += 1
                         self.model.cuda(self.device)
-                    a_k = max(self.lr/2 * (math.cos(math.pi * r_k) + 1), self.threshold)
-                    print('r_k: ', r_k)
-                    print('a_k: ', a_k)
+                    
                     # Do SGLD
                     params = self.model.parameters()
-                    optimizer = SGLD(params, a_k, self.sample_size)
+                    optimizer = SGLD(params, lr, self.lr0, self.sample_size)
                     pred = self.model(X)
                     loss = self.loss_fn(pred, y)
                     loss.backward()
                     optimizer.step()
                     optimizer.zero_grad()
-                    if iters % 100 == 0:
+                    if kk % 100 == 0:
                         self.test_it()
-                    iters+=1
+                        print('r: ', r)
+                        print('alpha: ', alpha)
+                        print('lr:', lr)
+                    kk+=1
 
             
         elif self.sampler_name == "sgld":
             params = self.model.parameters()
-            optimizer = SGLD(params, self.lr, self.sample_size)
-            iters = 1
+            optimizer = SGLD(params, self.lr0, self.lr0, self.sample_size)
+            kk = 1
             for t in range(self.epoches):
                 print(f"Epoch {t+1}\n-------------------------------")
                 for batch, (X, y) in enumerate(self.train_dataloader):
@@ -149,13 +171,14 @@ class mcmc_train_test(object):
                     loss.backward()
                     optimizer.step()
                     optimizer.zero_grad()
-                    if iters % 100 == 0:
+                    if kk % 100 == 0:
                         self.test_it()
+                    if kk % 10000 == 0:
                         print('save!')
                         self.model.cpu()
-                        torch.save(self.model.state_dict(),f'{self.res_dir}/model_{iters}.pt')
+                        torch.save(self.model.state_dict(),f'{self.res_dir}/model_{kk}.pt')
                         self.model.cuda(self.device)
-                    iters+=1
+                    kk +=1
             
         return self.Loss, self.Acc, self.Sparsity
                     
@@ -181,4 +204,5 @@ class mcmc_train_test(object):
         self.Acc.append(correct)
         if self.sparse:
             self.Sparsity.append(self.spar)
+            print(f"Sparsity: {self.spar} \n")
         print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
